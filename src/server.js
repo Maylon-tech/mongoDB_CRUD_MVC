05af4f719b91cea9bf2592dad35766625a2437dd
Maylon-tech <minatokiroisen77@gmail.com>
2023-12-07 13:20:41 +0900

    add usefull Modules

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home page'
+    })
+})
+
+app.use((req, res) => {
+    res.send("Sorry! Page Not Found.!!")
+})
+
+
+const port = process.env.PORT || 7676
+app.listen(port, () => console.log("Server is already Running!!"))
